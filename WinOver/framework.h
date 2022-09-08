f55650613fa2911552070a3e63fb354f4d242954
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-09-08 03:29:20 -0400

    Cleanup

--- WinOver/framework.h
+++ WinOver/framework.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <sdkddkver.h>
-#define NTDDI_VERSION NTDDI_WIN8   
-#define _WIN32_WINNT _WIN32_WINNT_WIN8   
-#define WINVER _WIN32_WINNT_WIN8   
+#define NTDDI_VERSION NTDDI_WIN8
+#define _WIN32_WINNT _WIN32_WINNT_WIN8
+#define WINVER _WIN32_WINNT_WIN8
 #define NOGDICAPMASKS
 #define NOVIRTUALKEYCODES
 #define NOSYSMETRICS
