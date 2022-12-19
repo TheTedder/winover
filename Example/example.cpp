718a55fd25fdc07dcf6f23204ed04db87fd72c67
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2022-12-19 17:46:37 -0500

    Set the minimum OS version to Windows 7.

--- Example/example.cpp
+++ Example/example.cpp
@@ -1,3 +1,8 @@
+#include <sdkddkver.h>
+#define NTDDI_VERSION NTDDI_WIN7
+#define _WIN32_WINNT _WIN32_WINNT_WIN7
+#define WINVER _WIN32_WINNT_WIN7
+
 #include <Windows.h>
 #include <Psapi.h>
 #include <Shlwapi.h>
